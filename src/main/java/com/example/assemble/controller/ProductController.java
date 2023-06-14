bf32b2178fc16da2c6e20f451dbc13ba9666ba4e
Jayklef <jerryarhawho@gmail.com>
2023-06-15 00:00:27 +0100

    ADD GET Product

--- src/main/java/com/example/assemble/controller/ProductController.java
+++ src/main/java/com/example/assemble/controller/ProductController.java
@@ -7,10 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -31,5 +28,11 @@ public class ProductController {
     public ResponseEntity<List<Product>> getProducts(){
         List<Product> products = productService.getProducts();
         return new ResponseEntity<>(products, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable("id") Long id){
+        Product product = productService.getProduct(id);
+        return new ResponseEntity<>(id, HttpStatus.FOUND);
     }
 }
