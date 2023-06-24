03a6d1e204b89a8fbca57c7adab54027416a95d0
Jayklef <jerryarhawho@gmail.com>
2023-06-24 16:01:02 +0100

    Exceptional Handler

---
+++ src/main/java/com/example/assemble/exception/ExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.example.assemble.exception;
+
+import com.example.assemble.dto.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+
+@ControllerAdvice
+public class ExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @org.springframework.web.bind.annotation.ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorResponse> ResourceNotFoundException(ResourceNotFoundException exception,
+                                                                   WebRequest request){
+
+        ErrorResponse error = new ErrorResponse();
+        error.setStatusCode(HttpStatus.NOT_FOUND.value());
+        error.setStatus(HttpStatus.NOT_FOUND);
+        error.setMessage(exception.getMessage());
+        error.setTimestamp(new Date());
+
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+
+    }
+}
