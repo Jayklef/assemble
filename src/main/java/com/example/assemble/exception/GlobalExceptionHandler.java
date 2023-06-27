73715344c23acffd03f6c004df99cdee2a6e339c
Jayklef <jerryarhawho@gmail.com>
2023-06-27 15:11:51 +0100

    renamed ExceptionHandler to GlobalExceptionHandler

--- src/main/java/com/example/assemble/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/assemble/exception/GlobalExceptionHandler.java
@@ -4,15 +4,16 @@ import com.example.assemble.dto.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Date;
 
 @ControllerAdvice
-public class ExceptionHandler extends ResponseEntityExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @org.springframework.web.bind.annotation.ExceptionHandler(ResourceNotFoundException.class)
+    @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ErrorResponse> ResourceNotFoundException(ResourceNotFoundException exception,
                                                                    WebRequest request){
 
